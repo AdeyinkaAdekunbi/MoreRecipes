2b07513a8e7a9ccf66676464a554a93387cd448a
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-10-25 23:10:25 +0100

    FEATURE: An API route that allows a user add a recipe to the platform

--- server/app.js
+++ server/app.js
@@ -10,8 +10,19 @@ app.use(logger('dev'));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('/', (req, res) => res.status(200).send({
+app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
+
+/** @returns {object} A JSON object containing the id and url of the added recipe
+ * @description An API route that allows a user add a recipe to the platform
+ */
+app.post('/api/recipes', (req, res) => res.status(201).send({
+  id: 1,
+  url: '/api/recipes/1'
+}));
+
+export default app;
