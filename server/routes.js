d72f4553113ae16b54559885b00a85c4e4f12c0c
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-10-27 00:37:20 +0100

    Feature: An API route that allows a user to post reviews of a recipe to the platform

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,13 @@
+import * as reviewController from './controllers/reviews';
+
 module.exports = (app) => {
+  /**
+  * Post a review for recipe
+  * @returns {object} A JSON object with the id and url of the review
+  * @description An API route that allows a user to post review
+  */
+  app.post('/api/reviews', reviewController.createReview);
+
   /**
    * @description API route to catch all unregistered GET calls
    */
