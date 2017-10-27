f98fb16901603a7bd6416e76c80249a8dc93a401
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-10-27 12:25:53 +0100

    CHORE: Added URL parameter validation for Deleting a Recipe

--- server/routes.js
+++ server/routes.js
@@ -1,30 +1,19 @@
+import validate from 'express-validation';
 import * as recipeController from './controllers/recipes';
+import deleteRecipe from './validations/deleteRecipe';
 
 module.exports = (app) => {
-  /**
-   * Add A Recipe
-   * @returns {object} A JSON object with the id and url of the added recipe
-   * @description An API route that allows a user add a recipe to the platform
-   */
-  app.post('/api/recipes', recipeController.createRecipe);
-
-  /**
-   * Modify A Recipe
-   * @returns {object} A JSON object with the updated recipe data
-   * @description An API route that allows a user to modify a recipe on platform
-   */
-  app.put('/api/recipes/:recipeId', recipeController.updateRecipe);
-
   /**
    * Delete A Recipe
    * @description An API route that allows a user to delete a recipe
    */
-  app.delete('/api/recipes/:recipeId', recipeController.deleteRecipe);  
+  app.delete('/api/recipes/:recipeId', validate(deleteRecipe),
+    recipeController.deleteRecipe);
 
   /**
    * @description API route to catch all unregistered GET calls
    */
-  app.get('*', (req, res) => res.status(200).send({
+  app.get('/', (req, res) => res.status(200).send({
     message: 'Welcome to More Recipes.',
   }));
 };
