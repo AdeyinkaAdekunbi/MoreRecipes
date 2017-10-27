8a29d041506c329c597ca8ebbfb8e7fde8d29ef5
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-10-27 12:56:55 +0100

    CHORE: Added URL parameter check for Getting Most upvoted Recipes

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,27 +1,17 @@
-import newRecipe from '../models/newRecipe';
-import updatedRecipe from '../models/updatedRecipe';
 import allRecipes from '../models/allRecipes';
-import mostUpvotedRecipe from '../models/mostUpvotedRecipes';
+import sortedRecipes from '../models/sortedRecipes';
 
 module.exports = {
-  createRecipe(req, res) {
-    // Return Dummy Data with HTTP CREATED
-    res.status(201).send(newRecipe());
-  },
-  updateRecipe(req, res) {
-    // Return Dummy Data with HTTP OK
-    res.status(200).send(updatedRecipe());
-  },
-  deleteRecipe(req, res) {
-    // Return Dummy Data with HTTP NO CONTENT
-    res.status(204).send();
-  },
   getRecipes(req, res) {
-    // Return Dummy Data with HTTP OK
-    res.status(200).send(allRecipes());
-  },
-  getMostUpvoted(req, res) {
-    // Return Dummy Data with HTTP OK
-    res.status(200).send(mostUpvotedRecipe(req.query));
+    const sortBy = req.query.sort;
+    const orderBy = req.query.order;
+
+    // Check if request has sort and order parameters 
+    // and return dummy upvoted data
+    if (sortBy && orderBy) {
+      res.status(200).send(sortedRecipes(req.query));
+    } else { // Unrecognised or No parameter is passed
+      res.status(200).send(allRecipes());
+    }
   }
 };
--- server/models/newReview.js
+++
@@ -1,8 +0,0 @@
-module.exports = () => {
-  const Data = {
-    id: 1,
-    url: '/api/recipes/1/reviews/1'
-  };
-
-  return Data;
-};
