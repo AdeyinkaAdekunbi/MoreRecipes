881ba7fdc398d605bc91d6d68a5c2cfbc0646b0d
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-10-27 12:31:29 +0100

    CHORE: Added validation for Get Recipes

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,20 +1,6 @@
-import newRecipe from '../models/newRecipe';
-import updatedRecipe from '../models/updatedRecipe';
 import allRecipes from '../models/allRecipes';
 
 module.exports = {
-  createRecipe(req, res) {
-    // Return Dummy Data with HTTP CREATED
-    res.status(201).send(newRecipe());
-  },
-  updateRecipe(req, res) {
-    // Return Dummy Data with HTTP OK
-    res.status(200).send(updatedRecipe());
-  },
-  deleteRecipe(req, res) {
-    // Return Dummy Data with HTTP NO CONTENT
-    res.status(204).send();
-  },
   getRecipes(req, res) {
     // Return Dummy Data with HTTP OK
     res.status(200).send(allRecipes());
