6305c5ad4b9a5b5251aa526032a86ce7f600c25a
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-10-31 15:12:08 +0100

    create user model for database

---
+++ server/database/models/user.js
@@ -0,0 +1,37 @@
+
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.string,
+      unique: true,
+      allowNull: false,
+    },
+  }, {
+    classMethods: {
+      associate(models) {
+        // associations can be defined here
+      },
+    },
+  });
+  return User;
+};
