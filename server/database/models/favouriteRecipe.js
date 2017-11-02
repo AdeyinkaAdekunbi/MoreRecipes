1a9e013abe7eab280a69b6782e066276c7d518a3
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-11-02 07:23:03 +0100

    Fix: Add model and migration for favourited  data

---
+++ server/database/models/favouriteRecipe.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const FavouriteRecipe = sequelize.define('FavouriteRecipe', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    recipeId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    }
+  });
+
+  FavouriteRecipe.associate = (models) => {
+    FavouriteRecipe.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'RESTRICT',
+    });
+
+    FavouriteRecipe.belongsTo(models.Recipe, {
+      foreignKey: 'recipeId',
+      onDelete: 'RESTRICT',
+    });
+  };
+
+  return FavouriteRecipe;
+};
