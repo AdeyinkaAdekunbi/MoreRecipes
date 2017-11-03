3ced0fbc8cfebef5c1c882b0624631cfababe456
AdeyinkaAdekunbi <kunbite15@gmail.com>
2017-11-03 09:04:03 +0100

    Setup database production

--- server/config/database.js
+++ server/config/database.js
@@ -1,20 +1,20 @@
 {
-  "development": {
-    "username": "postgres",
-    "password": "kunbi",
-    "database": "more-recipes",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  development: {
+    username: "postgres",
+    password: "kunbi",
+    database: "more-recipes",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: "postgres";
+    dialect: "postgres"
   }
 }
